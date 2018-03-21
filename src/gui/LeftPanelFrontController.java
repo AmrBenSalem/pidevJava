d45a5654625430ef077ea93fe9b0ac6cd9d9f6b0
Amr <bensalem.amr@gmail.com>
2018-03-21 16:32:19 +0100

    Changes

---
+++ src/gui/LeftPanelFrontController.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author Justpro
+ */
+public class LeftPanelFrontController implements Initializable {
+
+    @FXML
+    private AnchorPane anchorSideBar;
+    @FXML
+    private VBox vboxSideBar;
+    @FXML
+    private Pane paneSideBar;
+    @FXML
+    private Button buttonCoVoiturage;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
