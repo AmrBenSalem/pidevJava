5e20076a9946ffec9b93c5cfe6965c42a66cea96
Amr <bensalem.amr@gmail.com>
2018-03-22 16:45:28 +0100

    cc

--- src/gui/DashboardCoVoiturageController.java
+++ src/gui/DashboardCoVoiturageController.java
@@ -5,12 +5,18 @@
  */
 package gui;
 
+import com.jfoenix.controls.JFXDrawer;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
 /**
  * FXML Controller class
@@ -18,18 +24,30 @@ import javafx.scene.layout.AnchorPane;
  * @author Justpro
  */
 public class DashboardCoVoiturageController implements Initializable {
-    
-    @FXML
-    private AnchorPane anchorDashboardCoVoiturage;
+
     @FXML
     private Button btn;
+    @FXML
+    private AnchorPane anchor;
+    @FXML
+    private JFXDrawer drawerLeft;
+    @FXML
+    private JFXDrawer drawerTop;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        try {
+            //drawerLeft.open();
+            VBox leftMenu = null;
+            leftMenu = FXMLLoader.load(getClass().getResource("LeftMenuFront.fxml"));
+            
+            // drawerLeft.setSidePane((Node) leftMenu);
+        } catch (IOException ex) {
+            Logger.getLogger(DashboardCoVoiturageController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }    
     
 }
