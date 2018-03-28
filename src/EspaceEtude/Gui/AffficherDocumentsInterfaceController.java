7ebcd9068411a60f9db86b7f923b28db78451c02
lasda1 <35976032+lasda1@users.noreply.github.com>
2018-03-28 17:10:41 +0100

    all

---
+++ src/EspaceEtude/Gui/AffficherDocumentsInterfaceController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EspaceEtude.Gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author oussema
+ */
+public class AffficherDocumentsInterfaceController implements Initializable {
+
+    @FXML
+    private Button addButton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void startCrud(MouseEvent event) {
+    }
+    
+}
