d2849f03476a609c008a4567af3f93beb2e0c07f
lasda1 <35976032+lasda1@users.noreply.github.com>
2018-04-03 23:54:29 +0100

    allol

---
+++ src/EspaceEtude/Gui/AffficherDocumentsInterfaceController.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EspaceEtude.Gui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author oussema
+ */
+public class AffficherDocumentsInterfaceController implements Initializable {
+
+    @FXML
+    private Button addButton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void startCrud(MouseEvent event) throws IOException {
+          FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/EspaceEtude/Gui/ajouterDocument.fxml"));
+            // AfficherMatiereInterfaceController display= new AfficherMatiereInterfaceController();
+         Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root1));
+        ((Node)(event.getSource())).getScene().getWindow().hide();
+        
+        
+        stage.show();
+    }
+    
+}
